b55154b6ad8adce1347811f02ff2d45434a2c124
Michael Avila <me@michaelavila.com>
2015-12-22 23:50:06 -0800

    Install zsh and make it the default shell

--- manifests/site.pp
+++ manifests/site.pp
@@ -65,6 +65,7 @@ node default {
   include osx::keyboard::capslock_to_control
   include osx::no_network_dsstores
   include osx::safari::enable_developer_mode
+  include zsh
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
