8bd7d85c3d778cede58e8894a2f387446c7d2c6a
Brian Riley <brian@btriley.com>
2015-12-27 01:36:44 -0800

    Add my manifest
    Added my own file as well as the ruby repo for work.

--- manifests/site.pp
+++ manifests/site.pp
@@ -74,6 +74,7 @@ node default {
   include osx::keyboard::capslock_to_control
   include osx::no_network_dsstores
   include osx::safari::enable_developer_mode
+  include ruby
   include terraform
   include tmux
   include vagrant
